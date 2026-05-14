294ebadf2cce8312c758b171bc6bdd0ac347861d
Raj kumar Rabidas <152253071+RajkrRabidas@users.noreply.github.com>
2026-05-14 23:38:17 +0530

    Update app.js

--- functions/app.js
+++ functions/app.js
@@ -7,4 +7,8 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+app.get('/about', (req, res) => {
+  res.send('Hello! my name is Raj, I am a software developer');
+});
+
 module.exports.handler = serverless(app);
